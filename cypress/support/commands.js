1b61d517b1b0dff915a5742a62cb0b7f4991d844
tamasucirahmadani <tamasucirahmadani@gmail.com>
2025-01-19 20:01:23 +0700

    pesan

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -11,7 +11,18 @@
 //
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
-//
+Cypress.Commands.add('login', (email, password) => {
+    cy.visit('/customer/account/login');
+    cy.get('#email').type(email);
+    cy.get('#pass').type(password);
+    cy.get('button[type="submit"]').should('be.visible').click();
+  });
+  
+  Cypress.Commands.add('addToCart', (productName) => {
+    cy.visit('/');
+    cy.get('.product-item-link').contains(productName).click();
+    cy.get('#product-addtocart-button').click();
+  });
 //
 // -- This is a child command --
 // Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
@@ -23,3 +34,5 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+  
